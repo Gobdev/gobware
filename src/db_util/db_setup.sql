2999e978dd22e0a2d788bfe26d1e9b425aaa4c99
Johan Ribom <jribom@kth.se>
2017-08-12 17:17:52 +0200

    :construction: Change name of database from temporary name to gobware

--- src/db_util/db_setup.sql
+++ src/db_util/db_setup.sql
@@ -2,21 +2,21 @@ DO
 $body$
 BEGIN
    IF NOT EXISTS (
-       SELECT * FROM pg_catalog.pg_user WHERE usename = 'jap_proj_user')
+       SELECT * FROM pg_catalog.pg_user WHERE usename = 'gob_user')
    THEN
-      CREATE ROLE jap_proj_user LOGIN PASSWORD 'psqlisstupid';
+      CREATE ROLE gob_user LOGIN PASSWORD 'psqlisstupid';
    END IF;
 END
 $body$;
 
 CREATE TABLE IF NOT EXISTS sentences (
     id                SERIAL PRIMARY KEY,
-    originial         VARCHAR NOT NULL,
+    original          VARCHAR NOT NULL,
     hiragana          VARCHAR NOT NULL,
     translation       VARCHAR NOT NULL,
     context           VARCHAR
 );
-GRANT ALL PRIVILEGES ON TABLE sentences TO jap_proj_user;
+GRANT ALL PRIVILEGES ON TABLE sentences TO gob_user;
 
 CREATE TABLE IF NOT EXISTS kanji (
     kanji             VARCHAR PRIMARY KEY,
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS kanji (
     translation       VARCHAR NOT NULL,
     notes             VARCHAR
 );
-GRANT ALL PRIVILEGES ON TABLE kanji TO jap_proj_user;
+GRANT ALL PRIVILEGES ON TABLE kanji TO gob_user;
 
 CREATE TABLE IF NOT EXISTS jukugo (
     kanji             VARCHAR PRIMARY KEY,
@@ -33,4 +33,6 @@ CREATE TABLE IF NOT EXISTS jukugo (
     translation       VARCHAR NOT NULL,
     notes             VARCHAR
 );
-GRANT ALL PRIVILEGES ON TABLE jukugo TO jap_proj_user;
+GRANT ALL PRIVILEGES ON TABLE jukugo TO gob_user;
+
+GRANT ALL PRIVILEGES ON DATABASE gobware TO gob_user;
